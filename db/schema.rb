dfe304a860dbb94dbd7e54f880e0ea1dcacfb505
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-10-04 16:05:03 +0100

    editing devise views complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_110045) do
+ActiveRecord::Schema.define(version: 2018_10_04_134351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2018_10_04_110045) do
     t.text "body"
     t.string "image"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "blog_id"
+    t.integer "user_id"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
